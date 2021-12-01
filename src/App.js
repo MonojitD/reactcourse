744c65b327788b26d709584c7971a4a9919e8905
Monojit Deb <monojitdeb66@gmail.com>
2021-12-01 12:35:12 +0530

    Components Part 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import {
+  Navbar,
+  NavbarBrand
+} from "reactstrap";
+import Menu from './componentes/MenuComponent';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn More
-        </a>
-      </header>
+    <div>
+      <Navbar dark color="primary">
+        <NavbarBrand href="/">Restornate my-app</NavbarBrand>
+      </Navbar>
+      <Menu />
     </div>
   );
 }
